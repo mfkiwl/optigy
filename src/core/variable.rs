ea6d69d380d0e540abe4488e1ab5c26c7821c868
Lishen <pavel-panteluk@mail.ru>
2023-07-31 19:48:10 +0300

    upd

--- src/core/variable.rs
+++ src/core/variable.rs
@@ -15,3 +15,89 @@ where
 
     fn dim(&self) -> usize;
 }
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+
+    #[derive(Debug, Clone)]
+    pub struct VarA<R>
+    where
+        R: RealField,
+    {
+        pub val: Mat<R>,
+    }
+
+    impl<R> Variable<R> for VarA<R>
+    where
+        R: RealField,
+    {
+        fn local(&self, value: &Self) -> Mat<R>
+        where
+            R: RealField,
+        {
+            (self.val.as_ref() - value.val.as_ref()).clone()
+        }
+
+        fn retract(&mut self, delta: &MatRef<R>)
+        where
+            R: RealField,
+        {
+            self.val = self.val.clone() + delta.to_owned();
+        }
+
+        fn dim(&self) -> usize {
+            3
+        }
+    }
+    #[derive(Debug, Clone)]
+    pub struct VarB<R>
+    where
+        R: RealField,
+    {
+        pub val: Mat<R>,
+    }
+
+    impl<R> Variable<R> for VarB<R>
+    where
+        R: RealField,
+    {
+        fn local(&self, value: &Self) -> Mat<R>
+        where
+            R: RealField,
+        {
+            (self.val.as_ref() - value.val.as_ref()).clone()
+        }
+
+        fn retract(&mut self, delta: &MatRef<R>)
+        where
+            R: RealField,
+        {
+            self.val = self.val.clone() + delta.to_owned();
+        }
+
+        fn dim(&self) -> usize {
+            3
+        }
+    }
+
+    impl<R> VarA<R>
+    where
+        R: RealField,
+    {
+        pub fn new(v: R) -> Self {
+            VarA {
+                val: Mat::<R>::with_dims(3, 1, |_i, _j| v.clone()),
+            }
+        }
+    }
+    impl<R> VarB<R>
+    where
+        R: RealField,
+    {
+        pub fn new(v: R) -> Self {
+            VarB {
+                val: Mat::<R>::with_dims(3, 1, |_i, _j| v.clone()),
+            }
+        }
+    }
+}
--- src/core/variables.rs
+++ src/core/variables.rs
@@ -82,90 +82,10 @@ where
 }
 #[cfg(test)]
 mod tests {
+    use crate::core::variable::tests::{VarA, VarB};
+
     use super::*;
     use faer_core::{Mat, MatRef};
-
-    #[derive(Debug, Clone)]
-    struct VarA<R>
-    where
-        R: RealField,
-    {
-        val: Mat<R>,
-    }
-
-    impl<R> Variable<R> for VarA<R>
-    where
-        R: RealField,
-    {
-        fn local(&self, value: &Self) -> Mat<R>
-        where
-            R: RealField,
-        {
-            (self.val.as_ref() - value.val.as_ref()).clone()
-        }
-
-        fn retract(&mut self, delta: &MatRef<R>)
-        where
-            R: RealField,
-        {
-            self.val = self.val.clone() + delta.to_owned();
-        }
-
-        fn dim(&self) -> usize {
-            3
-        }
-    }
-    #[derive(Debug, Clone)]
-    struct VarB<R>
-    where
-        R: RealField,
-    {
-        val: Mat<R>,
-    }
-
-    impl<R> Variable<R> for VarB<R>
-    where
-        R: RealField,
-    {
-        fn local(&self, value: &Self) -> Mat<R>
-        where
-            R: RealField,
-        {
-            (self.val.as_ref() - value.val.as_ref()).clone()
-        }
-
-        fn retract(&mut self, delta: &MatRef<R>)
-        where
-            R: RealField,
-        {
-            self.val = self.val.clone() + delta.to_owned();
-        }
-
-        fn dim(&self) -> usize {
-            3
-        }
-    }
-
-    impl<R> VarA<R>
-    where
-        R: RealField,
-    {
-        fn new(v: R) -> Self {
-            VarA {
-                val: Mat::<R>::with_dims(3, 1, |_i, _j| v.clone()),
-            }
-        }
-    }
-    impl<R> VarB<R>
-    where
-        R: RealField,
-    {
-        fn new(v: R) -> Self {
-            VarB {
-                val: Mat::<R>::with_dims(3, 1, |_i, _j| v.clone()),
-            }
-        }
-    }
 
     #[test]
     fn add_variable() {
