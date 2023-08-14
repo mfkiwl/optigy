4bec025f797e86ecc56d412f0160165ce2022f4c
Pavel Panteliuk <pavel-panteluk@mail.ru>
2023-08-14 12:12:18 +0300

    upd

--- src/nonlinear/linearization.rs
+++ src/nonlinear/linearization.rs
@@ -1,7 +1,7 @@
 use std::ops::{Deref, SubAssign};
 
 use hashbrown::HashSet;
-use nalgebra::{DMatrix, DVector, RealField};
+use nalgebra::{DMatrix, DMatrixViewMut, DVector, RealField};
 use num::Float;
 
 use crate::core::{
@@ -130,6 +130,14 @@ fn linearzation_lower_hessian_single_factor<R, VC, FC>(
     let wht_Js_err = factors.jacobians_error_at(variables, f_index).unwrap();
     let wht_Js = wht_Js_err.jacobians;
     let wht_err = wht_Js_err.error;
+    let jacobians_view: Vec<DMatrixViewMut<R>> =
+        jacobians.iter_mut().map(|m| m.as_view_mut()).collect();
+    factors.weight_jacobians_error_in_place_at(
+        variables,
+        error.as_view_mut(),
+        &jacobians_view,
+        f_index,
+    );
 
     let stackJ = stack_matrix_col(&wht_Js);
 
