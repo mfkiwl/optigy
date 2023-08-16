8694f95f2e41124f34ab685d90fadd495e73f035
Pavel Panteliuk <pavel-panteluk@mail.ru>
2023-08-16 11:53:15 +0300

    upd

--- src/linear/sparse_cholesky_solver.rs
+++ src/linear/sparse_cholesky_solver.rs
@@ -1,5 +1,6 @@
 use std::{marker::PhantomData, time::Instant};
 
+use clarabel::{algebra, qdldl::QDLDLFactorisation};
 use nalgebra::{DVector, RealField};
 use nalgebra_sparse::{factorization::CscCholesky, CooMatrix, CscMatrix};
 use num::Float;
@@ -14,6 +15,25 @@ where
 {
     __marker: PhantomData<R>,
 }
+
+fn test_matrix_4x4() -> algebra::CscMatrix<f64> {
+    // A =
+    //[ 8.0  -3.0   2.0    ⋅ ]
+    //[  ⋅    8.0  -1.0    ⋅ ]
+    //[  ⋅     ⋅    8.0  -1.0]
+    //[  ⋅     ⋅     ⋅    1.0]
+    let Ap = vec![0, 1, 3, 6, 8];
+    let Ai = vec![0, 0, 1, 0, 1, 2, 2, 3];
+    let Ax = vec![8., -3., 8., 2., -1., 8., -1., 1.];
+    algebra::CscMatrix {
+        m: 4,
+        n: 4,
+        colptr: Ap,
+        rowval: Ai,
+        nzval: Ax,
+    }
+}
+
 impl<R> SparseLinearSolver<R> for SparseCholeskySolver<R>
 where
     R: RealField + Float + Default,
@@ -50,34 +70,71 @@ where
         //     }
         //     Err(_) => LinearSolverStatus::RankDeficiency,
         // }
+
+        // let start = Instant::now();
+        // let mut tri = TriMat::new((A.nrows(), A.ncols()));
+        // for (i, j, v) in A.triplet_iter() {
+        //     let v = v.to_f64().unwrap();
+        //     tri.add_triplet(i, j, v);
+        //     //make symmetry
+        //     if i != j {
+        //         tri.add_triplet(j, i, v);
+        //     }
+        // }
+        // let A: CsMat<_> = tri.to_csc();
+        // let duration = start.elapsed();
+        // println!("cholesky prepare time: {:?}", duration);
+        // let start = Instant::now();
+        // let ldlt = Ldl::new()
+        //     .check_symmetry(SymmetryCheck::DontCheckSymmetry)
+        //     .fill_in_reduction(FillInReduction::ReverseCuthillMcKee)
+        //     .numeric(A.view())
+        //     .unwrap();
+        // let duration = start.elapsed();
+        // let mut bv = Vec::<f64>::new();
+        // for i in 0..b.nrows() {
+        //     bv.push(b[i].to_f64().unwrap());
+        // }
+        // let sol = ldlt.solve(bv.as_slice());
+        // for i in 0..sol.len() {
+        //     x[i] = R::from_f64(sol[i]).unwrap();
+        // }
+        // println!("cholesky factor time: {:?}", duration);
         let start = Instant::now();
-        let mut tri = TriMat::new((A.nrows(), A.ncols()));
+        let mut coo = CooMatrix::<R>::zeros(A.nrows(), A.ncols());
         for (i, j, v) in A.triplet_iter() {
-            let v = v.to_f64().unwrap();
-            tri.add_triplet(i, j, v);
-            //make symmetry
-            if i != j {
-                tri.add_triplet(j, i, v);
-            }
+            //lower to upper
+            coo.push(j, i, *v);
         }
-        let A: CsMat<_> = tri.to_csc();
+        let A = &CscMatrix::from(&coo);
+        let (patt, vals) = A.clone().into_pattern_and_values();
+        let mut fvals = Vec::<f64>::new();
+        for v in vals {
+            fvals.push(v.to_f64().unwrap());
+        }
+        let A = algebra::CscMatrix::new(
+            A.nrows(),
+            A.ncols(),
+            patt.major_offsets().to_vec(),
+            patt.minor_indices().to_vec(),
+            fvals,
+        );
         let duration = start.elapsed();
         println!("cholesky prepare time: {:?}", duration);
+        assert!(A.check_format().is_ok());
         let start = Instant::now();
-        let ldlt = Ldl::new()
-            .check_symmetry(SymmetryCheck::DontCheckSymmetry)
-            .fill_in_reduction(FillInReduction::ReverseCuthillMcKee)
-            .numeric(A.view())
-            .unwrap();
-        let duration = start.elapsed();
-        println!("cholesky factor time: {:?}", duration);
+        let mut factors = QDLDLFactorisation::new(&A, None);
         let mut bv = Vec::<f64>::new();
         for i in 0..b.nrows() {
             bv.push(b[i].to_f64().unwrap());
         }
-        let sol = ldlt.solve(bv.as_slice());
-        for i in 0..sol.len() {
-            x[i] = R::from_f64(sol[i]).unwrap();
+        //solves in place
+        factors.solve(&mut bv);
+
+        let duration = start.elapsed();
+        println!("cholesky factor time: {:?}", duration);
+        for i in 0..bv.len() {
+            x[i] = R::from_f64(bv[i]).unwrap();
         }
         LinearSolverStatus::Success
     }
